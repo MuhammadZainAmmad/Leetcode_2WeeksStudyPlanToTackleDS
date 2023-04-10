146cb92a36025351a512eca7fb7aeff7d1c78d95
Muhammad Zain Ammad <zainammad05@gmail.com>
2023-04-10 06:09:59 +0500

    Started 'two weeks study plan to tackle DSA' with 'Data Structure 1'

---
+++ TwoWeeksStudyPlanToTackleDS/DataStructure1/ContainDuplicate/MyFirstAttempt_On2.py
@@ -0,0 +1,12 @@
+# My first solution 
+# Passing 65/71 test cases (Time limit exceeded)
+# Time: O(n^2), Space: O(1)
+class Solution(object):
+    def containsDuplicate(self, nums):
+        for i in nums: # O(n)
+            if nums.count(i) > 1: # O(n) 
+                return True 
+            else:
+                continue
+        return False
+    
