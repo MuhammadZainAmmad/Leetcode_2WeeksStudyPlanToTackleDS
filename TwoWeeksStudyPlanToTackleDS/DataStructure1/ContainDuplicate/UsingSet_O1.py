cd5b55d8e01b620dada63fcd4776c48b9d81766b
Muhammad Zain Ammad <zainammad05@gmail.com>
2023-04-10 06:12:13 +0500

    directory cleaned

--- TwoWeeksStudyPlanToTackleDS/DataStructure1/ContainDuplicate/UsingSet_O1.py
+++
@@ -1,8 +0,0 @@
-# Time: O(1), Space: O(1)
-class Solution(object): 
-    def containsDuplicate(self, nums):
-        uniqueNums = set(nums)
-        if len(uniqueNums) == len(nums): # O(1)
-            return False # b/c if duplicate then lengths would not be equal
-        else:
-            return True
